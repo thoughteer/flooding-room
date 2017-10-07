b9ce253cef4b464b65f8a29dca72cd3af7f6fc90
Iskander Sitdikov <thoughteer@gmail.com>
2017-10-07 22:00:28 +0300

    Attemp to refix sio

--- floodingroom/server/sio.py
+++ floodingroom/server/sio.py
@@ -32,12 +32,14 @@ class SIO(flask_socketio.SocketIO):
                     round_limit=3,
                     bet_limit=30)
             roomid = self.room.id
+            print("current room id: %s" % roomid)
             # get unique user id
             sid = flask.request.sid
             # try to add the player
             try:
                 player = self.room.add_player(sid)
-                flask_socketio.join_room(roomid, sid=sid)
+                flask_socketio.join_room(roomid)
+                print("client %s joined the room" % sid)
             except Exception as exc:
                 self.emit("decline", {"reason": str(exc)})
                 return
@@ -51,19 +53,21 @@ class SIO(flask_socketio.SocketIO):
             })
             # start if full
             if len(self.room.players) == self.room.players_limit:
+                print("room is full, start!")
                 self.room.start()
-                self.emit("start", {}, room=roomid)
+                self.emit("start", {}, room=roomid, broadcast=True)
                 return
             # wait for 30 seconds from room creation time
             start_time = self.room.timestamp + datetime.timedelta(seconds=30)
             now = datetime.datetime.utcnow()
             if now < start_time:
-                self.sleep((start_time - now).total_seconds())
+                print("sleeping")
+                flask_socketio.sleep((start_time - now).total_seconds())
             # check whether the game has already started
             if not self.room.is_started:
                 # start anyway
                 self.room.start()
-                self.emit("start", {}, room=roomid)
+                self.emit("start", {}, room=roomid, broadcast=True)
 
         @self.on("disconnect")
         def disconnect():
