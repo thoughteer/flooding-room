1794affbdebfc7febae87b8a3db4174f810b1911
Aleksandr Gorbunov <galexander113@gmail.com>
2017-10-07 20:56:38 +0300

    Add Player type

--- floodingroom/server/room.py
+++ floodingroom/server/room.py
@@ -26,6 +26,34 @@ class BetOverflowException(RoomException):
     pass
 
 
+class PlayerException(RoomException):
+    pass
+
+
+class Player(object):
+    class Type(object):
+        evil = 'Bad person'
+        good = 'Good person'
+
+    def __init__(self, id, type, bet_limit):
+        self.id = id
+        self.type = type
+        self.bet_limit = bet_limit
+        self._bet = None
+
+    @property
+    def bet(self):
+        if not self._bet:
+            return random.randint(0, self.bet_limit)
+        return self._bet
+
+    @bet.setter
+    def bet(self, points):
+        if points > self.bet_limit:
+            raise BetOverflowException('{id} makes too big bet, limit is {limit}'.format(id=id, limit=self.bet_limit))
+        self._bet = points
+
+
 class Room(object):
     def __init__(self, roomid, points_limit, players_limit, round_limit, bet_limit):
         self.id = roomid
@@ -35,36 +63,33 @@ class Room(object):
         self.bet_limit = bet_limit
         self.total = 0
         self.round = 0
-        self.players_bets = {}
+        self.players = {}
+        self.pidors = get_pidors(players_limit)
+
+    def get_player_type(self):
+        if len(self.players) == self.pidors[-1]:
+            self.pidors.pop()
+            return Player.Type.evil
+        return Player.Type.good
 
     def add_player(self, sid):
-        if sid in self.players_bets:
-            PlayerInRoomException('{sid} already in room {roomid}'.format(sid=sid, roomid=self.id))
-            return
-        if self.is_full():
+        if sid in self.players:
+            raise PlayerInRoomException('{sid} already in room {roomid}'.format(sid=sid, roomid=self.id))
+        if self.is_full:
             raise RoomOverflowException('{roomid} room is full'.format(roomid=self.id))
         if self.round != 0:
             raise GameStartedException('Game in {roomid} already started'.format(roomid=self.id))
 
-        self.players_bets[sid] = None
+        self.players[sid] = Player(sid, self.get_player_type(), self.bet_limit)
 
     def add_bet(self, sid, points):
-        if sid not in self.players_bets:
+        if sid not in self.players:
             raise MissedPlayerException('Bad {sid} for room {roomid}'.format(sid=sid, roomid=self.id))
-        if points > self.bet_limit:
-            raise BetOverflowException('{sid} makes too big bet, limit is {limit}'.format(sid=sid, limit=self.bet_limit))
-        self.players_bets[sid] = points
-
-    def add_random_bet(self, sid, a, b):
-        if sid not in self.players_bets:
-            raise MissedPlayerException('Bad {sid} for room {roomid}'.format(sid=sid, roomid=self.id))
-        self.players_bets[sid] = random.randint(a, b)
+        self.players[sid].bet = points
 
     def end_round(self):
         self.round += 1
-        for player, bet in self.players_bets.items():
-            if not bet:
-                self.add_random_bet(player, 0, self.bet_limit)
+        self.total += sum([player.bet for id, player in self.players.items()])
 
     @property
     def is_game_over(self):
@@ -72,4 +97,16 @@ class Room(object):
 
     @property
     def is_full(self):
-        return len(self.players_bets) >= self.players_limit
+        return len(self.players) >= self.players_limit
+
+
+def get_pidors(number):
+    num_pidors = number / 3
+    indexes = []
+    while num_pidors > 0:
+        pidor = random.randint(0, number)
+        if pidor not in indexes:
+            indexes.append(pidor)
+            num_pidors -= 1
+
+    return list(reversed(sorted(indexes)))
